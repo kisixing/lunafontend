5fc1026b0998eb1dac8d1892f4d329431bc8fdf9
lxf <lixiaofan.dev@gmail.com>
2019-10-03 14:39:42 +0800

    feat: memo component& Suit destroy enable

--- modules/packages/lmg/src/index.tsx
+++ modules/packages/lmg/src/index.tsx
@@ -1,40 +1,33 @@
 import React, { useEffect, useRef, useState } from 'react';
-import fakeData from './data';
+
 import { Suit } from './Suit';
 import { IBarTool } from './ScrollBar/useScroll';
 import ScrollBar from './ScrollBar';
-export default ({ data, }) => {
+export default ({ data,mutableSuitObject={suit:null} }:{data:object,mutableSuitObject:{suit:Suit}}) => {
   let barTool: IBarTool;
-
-  // data = data || fakeData;
-  // console.log(data);
+  let suit: Suit
   const canvas1 = useRef<HTMLCanvasElement>(null);
   const canvas2 = useRef<HTMLCanvasElement>(null);
   const canvasline = useRef<HTMLCanvasElement>(null);
   const box = useRef<HTMLDivElement>(null);
-
-  // const [playStatus, setPlayStatus] = useState(false);
-  const [suit, setSuit] = useState(null as Suit);
   useEffect(() => {
-
-    const rect = box.current.getBoundingClientRect();
-    const { width, height } = rect;
     const instance = new Suit(
       canvas1.current,
       canvas2.current,
       canvasline.current,
-      width,
-      height,
+      box.current,
       barTool
     );
-    setSuit(instance);
-    instance.onStatusChange = status => {
+    suit = instance
+    suit.onStatusChange = status => {
       console.log(status);
-      // setPlayStatus(status);
     };
-
+    suit.init(data)
+    mutableSuitObject.suit = suit
+    return () => {
+      suit.destroy()
+    }
   }, []);
-  data && suit && suit.init(data)
   return (
     <div style={{ width: '100%', height: '100%' }} ref={box}>
       <div
@@ -45,31 +38,29 @@ export default ({ data, }) => {
           height: '100%',
         }}
       >
-        <canvas ref={canvas1} width="1500" height="430">
+        <canvas ref={canvas1} >
           <p>Your browserdoes not support the canvas element.</p>
         </canvas>
         <canvas
           style={{ position: 'absolute', left: '0', top: '0' }}
           ref={canvasline}
-          width="1500"
-          height="430"
+
         >
           <p>Your browserdoes not support the canvas element.</p>
         </canvas>
         <canvas
           style={{ position: 'absolute', left: '0', top: '0' }}
           ref={canvas2}
-          width="1500"
-          height="430"
-          // onMouseDown={e => {
-          //   suit && suit.p.OnMouseDown(e.nativeEvent);
-          // }}
-          // onMouseMove={e => {
-          //   suit && suit.p.OnMouseMove(e.nativeEvent);
-          // }}
-          // onMouseUp={e => {
-          //   suit && suit.p.OnMouseUp(e.nativeEvent);
-          // }}
+
+        // onMouseDown={e => {
+        //   suit && suit.p.OnMouseDown(e.nativeEvent);
+        // }}
+        // onMouseMove={e => {
+        //   suit && suit.p.OnMouseMove(e.nativeEvent);
+        // }}
+        // onMouseUp={e => {
+        //   suit && suit.p.OnMouseUp(e.nativeEvent);
+        // }}
         >
           <p>Your browserdoes not support the canvas element.</p>
         </canvas>
