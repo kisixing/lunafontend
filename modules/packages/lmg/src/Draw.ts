555b695901a3170bfef6e470602d204d49042116
lxf <lixiaofan.dev@gmail.com>
2019-11-09 16:36:58 +0800

    etg text display

--- modules/packages/lmg/src/Draw.ts
+++ modules/packages/lmg/src/Draw.ts
@@ -1,10 +1,17 @@
 import { EventEmitter } from "@lianmed/utils";
 import { Drawer } from "./interface";
+let sid = 0;
 
 export default class Draw extends EventEmitter implements Drawer {
+    sid = sid++
     width: number
     height: number
     wrap: HTMLElement
+    constructor() {
+        super()
+        console.log('resize', this)
+    }
+    log = console.log.bind(console, this.constructor.name, this.sid)
     destroy() { }
     init(data: any) { }
     resize() {
@@ -12,5 +19,9 @@ export default class Draw extends EventEmitter implements Drawer {
         const { width, height } = rect;
         this.width = width
         this.height = height
+        this._resize()
+    }
+    _resize() {
+
     }
 }
