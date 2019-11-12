d28bf21ced6116c177a24cf76aec72b4df7917f2
lxf <lixiaofan.dev@gmail.com>
2019-11-12 10:20:55 +0800

    analyze loading

--- modules/packages/pages/src/Ctg/Analyse/CTGChart.tsx
+++ modules/packages/pages/src/Ctg/Analyse/CTGChart.tsx
@@ -4,13 +4,13 @@ import { Ctg as L } from '@lianmed/lmg';
 
 const CTGChart = (props: { ctgData: any }) => {
   const { ctgData } = props;
-  console.log('zzz',props)
+
   return (
     <Context.Consumer>
       {(value: any) => {
         return (
 
-          <L suitType={2} data={ctgData} mutableSuitObject={value} />
+          <L suitType={2} loading={ctgData.fhr1 === undefined} data={ctgData} mutableSuitObject={value} />
         )
       }}
     </Context.Consumer>
