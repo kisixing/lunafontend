00bf8c13029b4ac188a45f49ea18e187bb86a7e9
lxf <lixiaofan.dev@gmail.com>
2020-03-09 12:34:50 +0800

    type

--- modules/packages/f_types/lib/obvue/ctg-exams-data.d.ts
+++ modules/packages/f_types/lib/obvue/ctg-exams-data.d.ts
@@ -1,9 +1,13 @@
 export interface _ctg_exams_data {
-    id: number;
-    startTime: string;
-    endTime: string;
-    result: null;
-    note: string;
-    diagnosis: null;
-    report: null;
+    fhr1: string;
+    fhr2: string;
+    fhr3: string;
+    toco: string;
+    fm: string;
+    docid: string;
+    fetalnum: string;
+    fetalposition: any;
+    _fhr1: string;
+    _fhr2: string;
+    _fhr3: string;
 }
